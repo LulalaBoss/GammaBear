3b239d61335c164846e9d1bebb86ca947da63c68
LulalaBoss <katyhatesnake@gmail.com>
2015-08-04 15:47:51 -0700

    changes

--- Dish.java
+++ Dish.java
@@ -4,16 +4,28 @@ public class Dish
 {
 	private String name;		
 	private int prepTime;
+	private int quality;
 	
 	public Dish(String name, int pTime)
 	{
 		this.name = name;
         prepTime = pTime;
+		quality = 0;
 	}
 	
 	public String getName()
 	{
 		return name;
+	}
+	
+	public int getPrepTime()
+	{
+		return prepTime;
+	}
+	
+	public int getQuality()
+	{
+		return quality;		
 	}
 	
 }
--- Staff.java
+++ Staff.java
@@ -8,17 +8,40 @@ public class Staff
 	private int serviceExp;
 	private int cookingExp;
 	
-	public Staff(String fName, String lName)
+	private int shift;
+	
+	public Staff(String fName, String lName, int shift)
 	{
 		firstName = fName;
 		lastName = lName;
 		serviceExp = 0;
 		cookingExp = 0;
+		this.shift = shift;
 	}
 	
 	public String getName()
 	{
 		return firstName + " " + lastName;
+	}
+	
+	public String getShift()
+	{
+		String shiftString;
+		
+		switch(shift)
+		{
+			case 1: shiftString = "Morning";
+			        break;
+			case 2: shiftString = "Noon";
+			        break;
+			case 3: shiftString = "Evening";
+			        break;
+			case 4: shiftString = "Night";
+			        break;
+			default: shiftString = "Not Assigned";
+		}
+		
+		return shiftString;
 	}
 	
 }
